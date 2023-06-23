1a7a9990cb7694ef476907152814bd7f72f00e17
Nanhe Kumar Pandey <nanhe@apples-MacBook-Pro.local>
2023-06-23 18:52:41 +0530

    adjacency list representation of graph

---
+++ new.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+int32_t main() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	
+
+	return 0;
+}
